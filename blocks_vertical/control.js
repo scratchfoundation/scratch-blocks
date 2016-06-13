bf66362d1c6fff4cb48a5033081c17d30c3eb8c8
Tim Mickel <tim.mickel@gmail.com>
2016-06-13 12:12:33 -0400

    More blocks (#401)
    * Add "then" text and a dummy input to control_if
    
    * Add "then" text to if-else; add image to repeat
    
    * Reshuffle of control blocks to match Scratch 2.0
    
    * Add "wait until" block
    
    * Add "repeat until" block.
    
    * Add menu for "stop" block.
    
    * Add "clone" blocks
    
    * Add broadcast menu examples

--- blocks_vertical/control.js
+++ blocks_vertical/control.js
@@ -56,7 +56,8 @@ Blockly.Blocks['control_repeat'] = {
   init: function() {
     this.jsonInit({
       "id": "control_repeat",
-      "message0": "repeat %1 times %2 %3",
+      "message0": "repeat %1 %2 %3",
+      "lastDummyAlign0": "RIGHT",
       "args0": [
         {
           "type": "input_value",
@@ -64,11 +65,16 @@ Blockly.Blocks['control_repeat'] = {
           "check": "Number"
         },
         {
-          "type": "input_dummy"
-        },
-        {
           "type": "input_statement",
           "name": "SUBSTACK"
+        },
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_forever.svg",
+          "width": 10,
+          "height": 10,
+          "alt": "*",
+          "flip_rtl": true
         }
       ],
       "inputsInline": true,
@@ -89,12 +95,15 @@ Blockly.Blocks['control_if'] = {
   init: function() {
     this.jsonInit({
       "type": "control_if",
-      "message0": "if %1 %2",
+      "message0": "if %1 then %2 %3",
       "args0": [
         {
           "type": "input_value",
           "name": "CONDITION",
           "check": "Boolean"
+        },
+        {
+          "type": "input_dummy"
         },
         {
           "type": "input_statement",
@@ -119,12 +128,15 @@ Blockly.Blocks['control_if_else'] = {
   init: function() {
     this.jsonInit({
       "type": "control_if_else",
-      "message0": "if %1 %2 else %3 %4",
+      "message0": "if %1 then %2 %3 else %4 %5",
       "args0": [
         {
           "type": "input_value",
           "name": "CONDITION",
           "check": "Boolean"
+        },
+        {
+          "type": "input_dummy"
         },
         {
           "type": "input_statement",
@@ -148,6 +160,35 @@ Blockly.Blocks['control_if_else'] = {
   }
 };
 
+Blockly.Blocks['control_stop_menu'] = {
+  /**
+   * Stop drop-down menu.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit(
+      {
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "STOP_OPTION",
+            "options": [
+              ['all', 'ALL'],
+              ['this script', 'THIS'],
+              ['other scripts in sprite', 'OTHER']
+            ]
+          }
+        ],
+        "inputsInline": true,
+        "output": "String",
+        "colour": Blockly.Colours.control.primary,
+        "colourSecondary": Blockly.Colours.control.secondary,
+        "colourTertiary": Blockly.Colours.control.tertiary
+      });
+  }
+};
+
 Blockly.Blocks['control_stop'] = {
   /**
    * Block for stop all scripts.
@@ -156,8 +197,13 @@ Blockly.Blocks['control_stop'] = {
   init: function() {
     this.jsonInit({
       "id": "control_stop",
-      "message0": "stop all scripts",
+      "message0": "stop %1",
       "args0": [
+        {
+          "type": "input_value",
+          "name": "STOP_OPTION",
+          "check": "String"
+        }
       ],
       "inputsInline": true,
       "previousStatement": null,
@@ -187,6 +233,160 @@ Blockly.Blocks['control_wait'] = {
       "inputsInline": true,
       "previousStatement": null,
       "nextStatement": null,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['control_wait_until'] = {
+  /**
+   * Block to wait until a condition becomes true.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "wait until %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "CONDITION",
+          "check": "Boolean"
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['control_repeat_until'] = {
+  /**
+   * Block for repeat until a condition becomes true.
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "repeat until %1 %2 %3",
+      "lastDummyAlign0": "RIGHT",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "CONDITION",
+          "check": "Boolean"
+        },
+        {
+          "type": "input_statement",
+          "name": "SUBSTACK"
+        },
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_forever.svg",
+          "width": 10,
+          "height": 10,
+          "alt": "*",
+          "flip_rtl": true
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['control_start_as_clone'] = {
+  /**
+   * Block for "when I start as a clone" hat.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "control_start_as_clone",
+      "message0": "when I start as a clone",
+      "args0": [
+      ],
+      "inputsInline": true,
+      "nextStatement": null,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['control_create_clone_of_menu'] = {
+  /**
+   * Create-clone drop-down menu.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit(
+      {
+        "message0": "%1",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "CLONE_OPTION",
+            "options": [
+              ['myself', 'MYSELF']
+            ]
+          }
+        ],
+        "inputsInline": true,
+        "output": "String",
+        "colour": Blockly.Colours.control.primary,
+        "colourSecondary": Blockly.Colours.control.secondary,
+        "colourTertiary": Blockly.Colours.control.tertiary
+      });
+  }
+};
+
+Blockly.Blocks['control_create_clone_of'] = {
+  /**
+   * Block for "create clone of..."
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "id": "control_start_as_clone",
+      "message0": "create clone of %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "CLONE_OPTION",
+          "check": "String"
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['control_delete_this_clone'] = {
+  /**
+   * Block for "delete this clone."
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "delete this clone",
+      "args0": [
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
       "colour": Blockly.Colours.control.primary,
       "colourSecondary": Blockly.Colours.control.secondary,
       "colourTertiary": Blockly.Colours.control.tertiary
