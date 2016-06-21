991dba00fa4c8fa0d99927618a0cb278dc873b1c
Tim Mickel <tim.mickel@gmail.com>
2016-06-21 16:22:02 -0400

    Horizontal width calculation and notch flattening (#436)
    * Horizontal width calculation and notch flattening
    
    * Fix comment ("include width of notch in block width")

--- core/block_render_svg_horizontal.js
+++ core/block_render_svg_horizontal.js
@@ -355,6 +355,7 @@ Blockly.BlockSvg.prototype.getHeightWidth = function(opt_ignoreFields) {
   if (nextBlock) {
     var nextHeightWidth = nextBlock.getHeightWidth(opt_ignoreFields);
     width += nextHeightWidth.width;
+    width -= Blockly.BlockSvg.NOTCH_WIDTH; // Exclude width of connected notch.
     height = Math.max(height, nextHeightWidth.height);
   }
   return {height: height, width: width};
@@ -420,6 +421,7 @@ Blockly.BlockSvg.prototype.renderCompute_ = function() {
     height: 0,
     bayHeight: 0,
     bayWidth: 0,
+    bayNotchAtRight: true,
     fieldRadius: 0,
     startHat: false,
     endCap: false
@@ -438,6 +440,12 @@ Blockly.BlockSvg.prototype.renderCompute_ = function() {
         var bBox = linkedBlock.getHeightWidth(true);
         metrics.bayHeight = Math.max(metrics.bayHeight, bBox.height);
         metrics.bayWidth = Math.max(metrics.bayWidth, bBox.width);
+      }
+      var linkedBlock = input.connection.targetBlock();
+      if (linkedBlock && !linkedBlock.lastConnectionInStack()) {
+        metrics.bayNotchAtRight = false;
+      } else {
+        metrics.bayWidth -= Blockly.BlockSvg.NOTCH_WIDTH;
       }
     }
 
@@ -683,10 +691,12 @@ Blockly.BlockSvg.prototype.renderDrawBottom_ = function(steps,
                Blockly.BlockSvg.CORNER_RADIUS + ' 0 0,1 ' +
                Blockly.BlockSvg.CORNER_RADIUS + ',' +
                Blockly.BlockSvg.CORNER_RADIUS);
-    steps.push('v', metrics.bayHeight - (Blockly.BlockSvg.CORNER_RADIUS * 3) -
-      Blockly.BlockSvg.NOTCH_HEIGHT - 2 * Blockly.BlockSvg.GRID_UNIT);
-    steps.push(Blockly.BlockSvg.NOTCH_PATH_DOWN);
-    steps.push('v', 2.5 * Blockly.BlockSvg.GRID_UNIT);
+    if (metrics.bayNotchAtRight) {
+      steps.push('v', metrics.bayHeight - (Blockly.BlockSvg.CORNER_RADIUS * 3) -
+        Blockly.BlockSvg.NOTCH_HEIGHT - 2 * Blockly.BlockSvg.GRID_UNIT);
+      steps.push(Blockly.BlockSvg.NOTCH_PATH_DOWN);
+    }
+    steps.push('V', metrics.bayHeight + 2 * Blockly.BlockSvg.GRID_UNIT);
     steps.push('a', Blockly.BlockSvg.CORNER_RADIUS + ',' +
                Blockly.BlockSvg.CORNER_RADIUS + ' 0 0,0 ' +
                Blockly.BlockSvg.CORNER_RADIUS + ',' +
@@ -740,6 +750,9 @@ Blockly.BlockSvg.prototype.renderDrawRight_ = function(steps, connectionsXY, met
 
   if (this.nextConnection) {
     steps.push(Blockly.BlockSvg.NOTCH_PATH_UP);
+
+    // Include width of notch in block width.
+    this.width += Blockly.BlockSvg.NOTCH_WIDTH;
 
     // Create next block connection.
     var connectionX;
