3765f539ca2c2cdc25631036eaea11c2f0e9f26f
Tim Mickel <tim.mickel@gmail.com>
2016-09-22 16:24:56 -0400

    Stop block with mutator

--- blocks_vertical/control.js
+++ blocks_vertical/control.js
@@ -183,57 +183,41 @@ Blockly.Blocks['control_if_else'] = {
   }
 };
 
-Blockly.Blocks['control_stop_menu'] = {
-  /**
-   * Stop drop-down menu.
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.jsonInit(
-      {
-        "message0": "%1",
-        "args0": [
-          {
-            "type": "field_dropdown",
-            "name": "STOP_OPTION",
-            "options": [
-              ['all', 'all'],
-              ['this script', 'this script'],
-              ['other scripts in sprite', 'other scripts in sprite']
-            ]
-          }
-        ],
-        "inputsInline": true,
-        "output": "String",
-        "outputShape": Blockly.OUTPUT_SHAPE_ROUND,
-        "colour": Blockly.Colours.control.secondary,
-        "colourSecondary": Blockly.Colours.control.secondary,
-        "colourTertiary": Blockly.Colours.control.tertiary
-      });
-  }
-};
-
 Blockly.Blocks['control_stop'] = {
   /**
    * Block for stop all scripts.
    * @this Blockly.Block
    */
   init: function() {
-    this.jsonInit({
-      "id": "control_stop",
-      "message0": "stop %1",
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "STOP_OPTION"
-        }
-      ],
-      "inputsInline": true,
-      "previousStatement": null,
-      "colour": Blockly.Colours.control.primary,
-      "colourSecondary": Blockly.Colours.control.secondary,
-      "colourTertiary": Blockly.Colours.control.tertiary
+    var stopDropdown = new Blockly.FieldDropdown([
+        ['all', 'all'],
+        ['this script', 'this script'],
+        ['other scripts in sprite', 'other scripts in sprite']
+    ], function(option) {
+      if (option == 'other scripts in sprite') {
+        this.sourceBlock_.setNextStatement(true);
+      } else {
+        this.sourceBlock_.setNextStatement(false);
+      }
     });
+    this.appendDummyInput()
+        .appendField('stop')
+        .appendField(stopDropdown, 'STOP_OPTION');
+    this.setColour(Blockly.Colours.control.primary,
+      Blockly.Colours.control.secondary,
+      Blockly.Colours.control.tertiary
+    );
+    this.setPreviousStatement(true);
+  },
+  mutationToDom: function() {
+    var container = document.createElement('mutation');
+    var hasNext = (this.getFieldValue('STOP_OPTION') == 'other scripts in sprite');
+    container.setAttribute('hasnext', hasNext);
+    return container;
+  },
+  domToMutation: function(xmlElement) {
+    var hasNext = (xmlElement.getAttribute('hasnext') == 'true');
+    this.setNextStatement(hasNext);
   }
 };
 
