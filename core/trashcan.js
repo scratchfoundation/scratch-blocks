633427f79cd4ba56b215a187ee2bf164d771de4d
Tim Mickel <tim.mickel@gmail.com>
2016-09-26 17:45:01 -0400

    Drop goog.Timer

--- core/trashcan.js
+++ core/trashcan.js
@@ -26,7 +26,6 @@
 
 goog.provide('Blockly.Trashcan');
 
-goog.require('goog.Timer');
 goog.require('goog.dom');
 goog.require('goog.math');
 goog.require('goog.math.Rect');
@@ -222,7 +221,7 @@ Blockly.Trashcan.prototype.dispose = function() {
   }
   this.svgLid_ = null;
   this.workspace_ = null;
-  goog.Timer.clear(this.lidTask_);
+  clearTimeout(this.lidTask_);
 };
 
 /**
@@ -287,7 +286,7 @@ Blockly.Trashcan.prototype.setOpen_ = function(state) {
   if (this.isOpen == state) {
     return;
   }
-  goog.Timer.clear(this.lidTask_);
+  clearTimeout(this.lidTask_);
   this.isOpen = state;
   this.animateLid_();
 };
@@ -307,7 +306,7 @@ Blockly.Trashcan.prototype.animateLid_ = function() {
   var opacity = goog.math.lerp(0.4, 0.8, this.lidOpen_);
   this.svgGroup_.style.opacity = opacity;
   if (this.lidOpen_ > 0 && this.lidOpen_ < 1) {
-    this.lidTask_ = goog.Timer.callOnce(this.animateLid_, 20, this);
+    this.lidTask_ = setTimeout(this.animateLid_.bind(this), 20);
   }
 };
 
