934e8ff7d8b4798c4054bc64d1c7e88fa5406762
TheBrokenRail <connor24nolan@live.com>
2016-12-08 21:23:57 -0500

    Update default_toolbox.js

--- blocks_horizontal/default_toolbox.js
+++ blocks_horizontal/default_toolbox.js
@@ -124,6 +124,13 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
               '</shadow>'+
             '</value>'+
           '</block>'+
+          '<block type="motion_moveback">'+
+            '<value name="STEPS">'+
+              '<shadow type="math_number">'+
+                '<field name="NUM">10</field>'+
+              '</shadow>'+
+            '</value>'+
+          '</block>'+
           '<block type="motion_turnright">'+
             '<value name="DEGREES">'+
               '<shadow type="math_number">'+
