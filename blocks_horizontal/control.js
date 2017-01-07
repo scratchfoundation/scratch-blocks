2f6208da48c68e49e7942dd4dd593a062b57ba8c
TheBrokenRail <connor24nolan@live.com>
2017-01-07 16:35:35 -0500

    Fix Alts

--- blocks_horizontal/control.js
+++ blocks_horizontal/control.js
@@ -50,7 +50,7 @@ Blockly.Blocks['control_repeat'] = {
           "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_repeat.svg",
           "width": 40,
           "height": 40,
-          "alt": "*",
+          "alt": "Repeat",
           "flip_rtl": true
         },
         {
@@ -90,52 +90,12 @@ Blockly.Blocks['control_forever'] = {
           "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_forever.svg",
           "width": 40,
           "height": 40,
-          "alt": "*",
+          "alt": "Forever",
           "flip_rtl": true
         }
       ],
       "inputsInline": true,
       "previousStatement": null,
-      "category": Blockly.Categories.control,
-      "colour": Blockly.Colours.control.primary,
-      "colourSecondary": Blockly.Colours.control.secondary,
-      "colourTertiary": Blockly.Colours.control.tertiary
-    });
-  }
-};
-
-Blockly.Blocks['control_repeat'] = {
-  /**
-   * Block for repeat n times (external number).
-   * https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#so57n9
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.jsonInit({
-      "id": "control_repeat",
-      "message0": "%1 %2 %3",
-      "args0": [
-        {
-          "type": "input_statement",
-          "name": "SUBSTACK"
-        },
-        {
-          "type": "field_image",
-          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_repeat.svg",
-          "width": 40,
-          "height": 40,
-          "alt": "*",
-          "flip_rtl": true
-        },
-        {
-          "type": "input_value",
-          "name": "TIMES",
-          "check": "Number"
-        }
-      ],
-      "inputsInline": true,
-      "previousStatement": null,
-      "nextStatement": null,
       "category": Blockly.Categories.control,
       "colour": Blockly.Colours.control.primary,
       "colourSecondary": Blockly.Colours.control.secondary,
