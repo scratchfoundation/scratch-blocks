6bbf178c3fcc171d29550f6e8eb007450798b53c
Yu <bigeyex@gmail.com>
2017-04-20 20:24:35 +0000

    Build for 06fa507dbbb989df09e7f18b8dfed475d8af664d

--- playgrounds/core/flyout_vertical.js
+++ playgrounds/core/flyout_vertical.js
@@ -208,7 +208,7 @@ Blockly.VerticalFlyout.prototype.getMetrics_ = function() {
     viewWidth: viewWidth,
     contentHeight: optionBox.height * this.workspace_.scale + 2 * this.MARGIN,
     contentWidth: optionBox.width * this.workspace_.scale + 2 * this.MARGIN,
-    viewTop: -this.workspace_.scrollY,
+    viewTop: -this.workspace_.scrollY + optionBox.y,
     viewLeft: -this.workspace_.scrollX,
     contentTop: optionBox.y,
     contentLeft: optionBox.x,
