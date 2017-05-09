64046b26089f951fa7fcc800948d46403b786504
Paul Kaplan <pkaplan@media.mit.edu>
2017-05-09 15:11:02 -0400

    Add back accidentally removed files

---
+++ shim/blockly_compressed_horizontal-blocks_compressed.js
@@ -0,0 +1,1 @@
+module.exports = require('imports?Blockly=./shim/blockly_compressed_horizontal.Blockly!exports?Blockly!../blocks_compressed');
---
+++ shim/blockly_compressed_vertical-blocks_compressed.js
@@ -0,0 +1,1 @@
+module.exports = require('imports?Blockly=./shim/blockly_compressed_vertical.Blockly!exports?Blockly!../blocks_compressed');
