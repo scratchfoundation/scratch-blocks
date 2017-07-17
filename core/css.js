c6558fa2d321ebaa31989fee3ab9decfc1782d84
Paul Kaplan <pkaplan@media.mit.edu>
2017-07-17 08:10:04 -0400

    Disable text selection on blocks and categories

--- core/css.js
+++ core/css.js
@@ -501,7 +501,7 @@ Blockly.Css.CONTENT = [
     Don't allow users to select text.  It gets annoying when trying to
     drag a block and selected text moves instead.
   */
-  '.blocklySvg text, .blocklyBlockDragSurface text {',
+  '.blocklySvg text, .blocklyBlockDragSurface text, .blocklyFlyout text, .blocklyToolboxDiv text {',
     'user-select: none;',
     '-moz-user-select: none;',
     '-webkit-user-select: none;',
