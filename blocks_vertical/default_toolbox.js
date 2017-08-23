a45d8ad6ab41a910c5ede79ae451db78c7149a7f
Ken <kenny2minecraft@gmail.com>
2017-08-23 21:54:15 +0800

    Add [string] contains [string] block (#1045)
    * Add [string] contains [string] block
    
    * Add [string] contains [string] to palette
    
    * Add second input 😒
    
    * Update as per requested changes

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -580,6 +580,18 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
         '</shadow>'+
       '</value>'+
     '</block>'+
+    '<block type="operator_contains" id="operator_contains">'+
+      '<value name="STRING1">'+
+        '<shadow type="text">'+
+          '<field name="TEXT">hello</field>'+
+        '</shadow>'+
+      '</value>'+
+      '<value name="STRING2">'+
+        '<shadow type="text">'+
+          '<field name="TEXT">world</field>'+
+        '</shadow>'+
+      '</value>'+
+    '</block>'+
     '<block type="operator_mod" id="operator_mod">'+
       '<value name="NUM1">'+
         '<shadow type="math_number">'+
