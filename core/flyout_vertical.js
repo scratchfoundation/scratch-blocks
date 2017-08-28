186d1983af6b69ecdbc2a1463db59f44ee49eb5c
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-08-28 14:33:55 -0400

    Prevent scroll locking up due to scroll target being too low

--- core/flyout_vertical.js
+++ core/flyout_vertical.js
@@ -338,6 +338,13 @@ Blockly.VerticalFlyout.prototype.scrollToStart = function() {
  */
 Blockly.VerticalFlyout.prototype.scrollTo = function(pos) {
   this.scrollTarget = pos * this.workspace_.scale;
+
+  // Make sure not to set the scroll target below the lowest point we can
+  // scroll to, i.e. the content height minus the view height
+  var contentHeight = this.workspace_.getMetrics().contentHeight;
+  var viewHeight = this.workspace_.getMetrics().viewHeight;
+  this.scrollTarget = Math.min(this.scrollTarget, contentHeight - viewHeight);
+
   this.step();
 };
 
