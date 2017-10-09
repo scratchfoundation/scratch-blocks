c838ca3a8756601d751877954cb773e20ebaef58
Rachel Fenichel <fenichel@google.com>
2017-10-09 12:04:59 -0700

    Add a bulkUpdate flag to avoid expensive operations (such as toolbox refresh) during workspace loading.

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -151,6 +151,14 @@ Blockly.WorkspaceSvg.prototype.isMutator = false;
 Blockly.WorkspaceSvg.prototype.resizesEnabled_ = true;
 
 /**
+ * Whether this workspace is in the middle of a bulk update.
+ * Turn on during batch operations for a performance improvement.
+ * @type {boolean}
+ * @private
+ */
+Blockly.WorkspaceSvg.prototype.isBulkUpdating_ = false;
+
+/**
  * Current horizontal scrolling offset in pixel units.
  * @type {number}
  */
@@ -1016,7 +1024,8 @@ Blockly.WorkspaceSvg.prototype.paste = function(xmlBlock) {
  * @private
  */
 Blockly.WorkspaceSvg.prototype.refreshToolboxSelection_ = function() {
-  if (this.toolbox_ && this.toolbox_.flyout_ && !this.currentGesture_) {
+  if (this.toolbox_ && this.toolbox_.flyout_ && !this.currentGesture_ &&
+     !this.isBulkUpdating_) {
     this.toolbox_.refreshSelection();
   }
 };
@@ -1797,12 +1806,34 @@ Blockly.WorkspaceSvg.prototype.setResizesEnabled = function(enabled) {
 };
 
 /**
+ * Set whether this workspace is currently doing a bulk update.
+ * A bulk update pauses workspace resizing but also pauses other expensive
+ * operations, such as refreshing the toolbox as variables are added and
+ * removed.
+ * @param {boolean} enabled True if a bulk update is starting, false if a bulk
+ *     update is ending.
+ * @package
+ */
+Blockly.WorkspaceSvg.prototype.setBulkUpdate = function(enabled) {
+  // This will trigger a resize if necessary.
+  this.setResizesEnabled(enabled);
+  var stoppedUpdating = (this.isBulkUpdating_ && !enabled);
+  this.isBulkUpdating_ = stoppedUpdating;
+  if (stoppedUpdating) {
+    // Refresh the toolbox.
+    if (this.toolbox_) {
+      this.toolbox_.refreshSelection();
+    }
+  }
+};
+
+/**
  * Dispose of all blocks in workspace, with an optimization to prevent resizes.
  */
 Blockly.WorkspaceSvg.prototype.clear = function() {
-  this.setResizesEnabled(false);
+  this.setBulkUpdate(true);
   Blockly.WorkspaceSvg.superClass_.clear.call(this);
-  this.setResizesEnabled(true);
+  this.setBulkUpdate(false);
 };
 
 /**
