f125e83bd6597184cec3bf3ba3c73b90c0963088
Rachel <fenichel@google.com>
2017-10-17 16:50:37 +0000

    Build for 4783e7fd9d4f58f2f1c99f85ce2816ffc4b5b7ed

--- playgrounds/tests/jsunit/xml_test.js
+++ playgrounds/tests/jsunit/xml_test.js
@@ -297,14 +297,17 @@ function test_blockToDom_fieldToDom_trivial() {
 
 function test_blockToDom_fieldToDom_defaultCase() {
   xmlTest_setUpWithMockBlocks();
-  setUpMockMethod(mockControl_, Blockly.utils, 'genUid', null, ['1', '1']);
-  workspace.createVariable('name1');
-  var block = new Blockly.Block(workspace, 'field_variable_test_block');
-  block.inputList[0].fieldRow[0].setValue('name1');
-  var resultFieldDom = Blockly.Xml.blockToDom(block).childNodes[0];
-  // Expect type is '' and id is '1' since we don't specify type and id.
-  xmlTest_checkVariableFieldDomValues(resultFieldDom, 'VAR', '', '1', 'name1');
-  xmlTest_tearDownWithMockBlocks();
+  try {
+    setUpMockMethod(mockControl_, Blockly.utils, 'genUid', null, ['1', '1']);
+    workspace.createVariable('name1');
+    var block = new Blockly.Block(workspace, 'field_variable_test_block');
+    block.inputList[0].fieldRow[0].setValue('name1');
+    var resultFieldDom = Blockly.Xml.blockToDom(block).childNodes[0];
+    // Expect type is '' and id is '1' since we don't specify type and id.
+    xmlTest_checkVariableFieldDomValues(resultFieldDom, 'VAR', '', '1', 'name1');
+  } finally {
+    xmlTest_tearDownWithMockBlocks();
+  }
 }
 
 function test_blockToDom_fieldToDom_notAFieldVariable() {
