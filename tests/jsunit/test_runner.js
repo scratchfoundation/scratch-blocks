660503193fbcfc786408df41d17d8b003d11311f
Paul Kaplan <pkaplan@media.mit.edu>
2017-10-18 09:06:33 -0400

    Get unit tests running on travis

--- tests/jsunit/test_runner.js
+++ tests/jsunit/test_runner.js
@@ -5,31 +5,36 @@ var options = {
     }
 };
 
+// Parse jsunit html report, exit(1) if there are any failures.
+var testHtml = function (htmlString) {
+  var regex = /[\d]+\spassed,\s([\d]+)\sfailed./i;
+  var numOfFailure = regex.exec(htmlString)[1];
+  var regex2 = /Unit Tests for .*]/;
+  var testStatus = regex2.exec(htmlString)[0];
+  console.log("============Unit Test Summary=================");
+  console.log(testStatus);
+  var regex3 = /\d+ passed,\s\d+ failed/;
+  var detail = regex3.exec(htmlString)[0];
+  console.log(detail);
+  console.log("============Unit Test Summary=================");
+  if (parseInt(numOfFailure) !== 0) {
+    console.log(htmlString);
+    process.exit(1);
+  }
+};
+
 var path = process.cwd();
-//TODO: change pause to waitunitl
 var browser = webdriverio
     .remote(options)
-    .init()
-    .url("file://" + path + "/tests/jsunit/index.html").pause(5000);
-
+    .init();
 
 browser
-.getHTML('#closureTestRunnerLog')
-.then(function(result) {
-   // call js to parse html
-   var regex = /[\d]+\spassed,\s([\d]+)\sfailed./i;
-   var numOfFailure = regex.exec(result)[1];
-  var regex2 = /Unit Tests for Blockly .*]/;
-  var testStatus = regex2.exec(result)[0];
-  console.log("============Blockly Unit Test Summary=================");
-   console.log(testStatus);
-   var regex3 = /\d+ passed,\s\d+ failed/;
-   var detail = regex3.exec(result)[0];
-   console.log(detail);
-  console.log("============Blockly Unit Test Summary=================");
-   if ( parseInt(numOfFailure) !== 0) {
-     console.log(result);
-     process.exit(1);
-    }
-})
-.catch(function(err) { console.log(err); process.exit(1); });
+  .url("file://" + path + "/tests/jsunit/vertical_tests.html").pause(5000)
+  .getHTML('body').then(testHtml)
+  .url("file://" + path + "/tests/jsunit/horizontal_tests.html").pause(5000)
+  .getHTML('body').then(testHtml)
+  .catch(function(err) {
+    console.log(err);
+    browser.end();
+    process.exit(1);
+  });
