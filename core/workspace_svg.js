2996c86c539f90841eaf5370f4958bab1362aaf9
picklesrus <picklesrus@users.noreply.github.com>
2017-10-24 12:40:22 -0700

    Do not refresh the toolbox when a new variable is created on the work… (#1166)
    * Do not refresh the toolbox when a new variable is created on the workspace but it already exists in the variable map.  This gets rid of some calls to Toolbox.refreshSelection. e.g. when you drag a variable block out of the toolbox, we were refreshing the toolbox.  It will also help with xml loading for workspaces using multiple of the same variable reporter.  More needs to be done to solve #879, but this helps a little.
    
    * Fix typo

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -1067,9 +1067,14 @@ Blockly.WorkspaceSvg.prototype.deleteVariableById = function(id) {
  * @package
  */
 Blockly.WorkspaceSvg.prototype.createVariable = function(name, opt_type, opt_id) {
+  var variableInMap = (this.getVariable(name) != null);
   var newVar = Blockly.WorkspaceSvg.superClass_.createVariable.call(this, name,
     opt_type, opt_id);
-  this.refreshToolboxSelection_();
+  // For performance reasons, only refresh the the toolbox for new variables.
+  // Variables that already exist should already be there.
+  if (!variableInMap) {
+    this.refreshToolboxSelection_();
+  }
   return newVar;
 };
 
