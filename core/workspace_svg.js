65500aee7e8ed1320d2b88a859e57bb6bff81638
picklesrus <picklesrus@users.noreply.github.com>
2017-10-24 09:43:00 -0700

    Fix typo

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -1079,7 +1079,7 @@ Blockly.WorkspaceSvg.prototype.createVariable = function(name, opt_type, opt_id)
   var variableInMap = (this.getVariable(name) != null);
   var newVar = Blockly.WorkspaceSvg.superClass_.createVariable.call(this, name,
     opt_type, opt_id);
-  // For performance reaons, only refresh the the toolbox for new variables.
+  // For performance reasons, only refresh the the toolbox for new variables.
   // Variables that already exist should already be there.
   if (!variableInMap) {
     this.refreshToolboxSelection_();
