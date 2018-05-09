8bf1baabd24b5f37ed6dbe5b0d2ffa114a48beac
Michael "Z" Goddard <mzgoddard@gmail.com>
2018-05-09 17:37:02 -0400

    Fix the uncompressed node_modules BLOCKLY_BOOT check

--- build.py
+++ build.py
@@ -134,7 +134,7 @@ window.BLOCKLY_BOOT = function() {
       alert('Error: Closure not found.  Read this:\\n' +
             'developers.google.com/blockly/guides/modify/web/closure');
     }
-    if (window.BLOCKLY_DIR.search(/node_modules/)) {
+    if (window.BLOCKLY_DIR.search(/node_modules/) > -1) {
       dir = '..';
     } else {
       dir = window.BLOCKLY_DIR.match(/[^\\/]+$/)[0];
