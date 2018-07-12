5659036d91d122898fb098fddb79dd9b2b4c2838
chrisgarrity <chrisg@media.mit.edu>
2018-07-12 10:45:12 -0400

    Separate updating translations from generating src.
    * setLocale uses Object.assign so any new keys in Blockly.Msg are maintained after changing locale. I.e. if a translation doesn’t exist in the translation English default will be used. In theory, it could end up with something other than English if the key is in one translation, but not another.
    However, it’s unlikely as we’re pulling the translations from transifex and either they will all have the key (if the key exists in the `en.json` source on transifex), or none of them will have the key.
    
    * translate script only generates `en` sources.
    * translate:update script generates new scratch_msgs.js file. It will fail if the keys in the downloaded files don’t match the ones in messages.

--- core/scratch_msgs.js
+++ core/scratch_msgs.js
@@ -55,7 +55,7 @@ Blockly.ScratchMsgs.currentLocale_ = 'en';
 Blockly.ScratchMsgs.setLocale = function(locale) {
   if (Object.keys(Blockly.ScratchMsgs.locales).includes(locale)) {
     Blockly.ScratchMsgs.currentLocale_ = locale;
-    Blockly.Msg = Blockly.ScratchMsgs.locales[locale];
+    Blockly.Msg = Object.assign({}, Blockly.Msg, Blockly.ScratchMsgs.locales[locale]);
   } else {
     // keep current locale
     console.warn('Ignoring unrecognized locale: ' + locale);
