1cc4b1b2e6f0488d6a862f142eb420a51043c6cf
chrisgarrity <chrisg@media.mit.edu>
2018-07-16 19:05:27 +0000

    Build for c9c381dfe311acc9d34411a09762f69fa196020d

--- playgrounds/core/toolbox.js
+++ playgrounds/core/toolbox.js
@@ -86,10 +86,10 @@ Blockly.Toolbox = function(workspace) {
 
 /**
  * Width of the toolbox, which changes only in vertical layout.
- * This is the sum of the width of the flyout (250) and the category menu (52).
+ * This is the sum of the width of the flyout (250) and the category menu (60).
  * @type {number}
  */
-Blockly.Toolbox.prototype.width = 302;
+Blockly.Toolbox.prototype.width = 310;
 
 /**
  * Height of the toolbox, which changes only in horizontal layout.
