4eec9df9c0a8361928dfa53c488354484218dd1a
DD <liudi08@gmail.com>
2018-08-08 20:52:17 -0400

    Fix lint

--- core/blockly.js
+++ core/blockly.js
@@ -179,10 +179,10 @@ Blockly.svgResize = function(workspace) {
  * @param {!Event} e Key down event.
  * @private
  */
- // TODO (https://github.com/google/blockly/issues/1998) handle cases where there are multiple workspaces
- // and non-main workspaces are able to accept input.
+// TODO (https://github.com/google/blockly/issues/1998) handle cases where there are multiple workspaces
+// and non-main workspaces are able to accept input.
 Blockly.onKeyDown_ = function(e) {
-    if (Blockly.mainWorkspace.options.readOnly || Blockly.utils.isTargetInput(e)
+  if (Blockly.mainWorkspace.options.readOnly || Blockly.utils.isTargetInput(e)
       || (Blockly.mainWorkspace.rendered && !Blockly.mainWorkspace.isVisible())) {
     // No key actions on readonly workspaces.
     // When focused on an HTML text input widget, don't trap any keys.
