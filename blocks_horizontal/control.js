4ee43c697b61a4116e5a76ea77a5a942d95e358f
Alan <Alan.zg@qq.com>
2019-01-10 16:45:08 +0800

    remove two control_repeat

--- blocks_horizontal/control.js
+++ blocks_horizontal/control.js
@@ -104,46 +104,6 @@ Blockly.Blocks['control_forever'] = {
   }
 };
 
-Blockly.Blocks['control_repeat'] = {
-  /**
-   * Block for repeat n times (external number).
-   * https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#so57n9
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.jsonInit({
-      "id": "control_repeat",
-      "message0": "%1 %2 %3",
-      "args0": [
-        {
-          "type": "input_statement",
-          "name": "SUBSTACK"
-        },
-        {
-          "type": "field_image",
-          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/control_repeat.svg",
-          "width": 40,
-          "height": 40,
-          "alt": "*",
-          "flip_rtl": true
-        },
-        {
-          "type": "input_value",
-          "name": "TIMES",
-          "check": "Number"
-        }
-      ],
-      "inputsInline": true,
-      "previousStatement": null,
-      "nextStatement": null,
-      "category": Blockly.Categories.control,
-      "colour": Blockly.Colours.control.primary,
-      "colourSecondary": Blockly.Colours.control.secondary,
-      "colourTertiary": Blockly.Colours.control.tertiary
-    });
-  }
-};
-
 Blockly.Blocks['control_stop'] = {
   /**
    * Block for stop all scripts.
