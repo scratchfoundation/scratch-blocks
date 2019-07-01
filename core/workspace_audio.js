db222411bf0cd1b7879f68d3d9201928b4eff725
Mathias Søholm <mss@trifork.com>
2019-07-01 08:49:38 +0200

    Fixed indentation

--- core/workspace_audio.js
+++ core/workspace_audio.js
@@ -119,7 +119,7 @@ Blockly.WorkspaceAudio.prototype.preload = function() {
       // If we don't wait for the play request to complete before calling pause() we will get an exception:
       // Uncaught (in promise) DOMException: The play() request was interrupted by a call to pause().
       // See more: https://developers.google.com/web/updates/2017/06/play-request-was-interrupted
-        playPromise.then(sound.pause).catch(function() {
+      playPromise.then(sound.pause).catch(function() {
         // Play without user interaction was prevented.
       });
     } else {
