ea68b1c25f055288e2ee39f846c17e5861f1a01f
Aaron Dodson <adodson@google.com>
2024-04-19 14:39:33 -0700

    fix: load the continuous toolbox (#31)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import * as Blockly from 'blockly/core';
 import '../blocks_common/math.js';
 import '../blocks_vertical/vertical_extensions.js';
 import '../blocks_vertical/control.js';
@@ -15,9 +16,26 @@ import '../blocks_vertical/operators.js';
 import '../blocks_vertical/sensing.js';
 import '../blocks_vertical/sound.js';
 import * as scratchBlocksUtils from '../core/scratch_blocks_utils.js';
+import {
+  ContinuousToolbox,
+  ContinuousFlyout,
+  ContinuousMetrics,
+} from '@blockly/continuous-toolbox';
 
 export * from 'blockly';
 export * from './categories.js';
 export * from '../core/colours.js';
 export * from '../msg/scratch_msgs.js';
 export {scratchBlocksUtils};
+
+export function inject(container, options) {
+  Object.assign(options, {
+    plugins: {
+      toolbox: ContinuousToolbox,
+      flyoutsVerticalToolbox: ContinuousFlyout,
+      metricsManager: ContinuousMetrics,
+    },
+  });
+  const workspace = Blockly.inject(container, options);
+  return workspace;
+}
