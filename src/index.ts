75e6e6c51290e1961e9bc49d8e57364c580a53b4
Aaron Dodson <adodson@google.com>
2024-10-15 14:19:25 -0700

    refactor: Add support for Typescript (#221)
    * chore: add Typescript dependencies
    
    * chore: add tsconfig.json
    
    * chore: update webpack.config.js for TS compatibility
    
    * chore: convert index.js to TS

--- src/index.ts
+++ src/index.ts
@@ -83,7 +83,7 @@ export {
   StatusButtonState,
 } from "./status_indicator_label.js";
 
-export function inject(container, options) {
+export function inject(container: Element, options: Blockly.BlocklyOptions) {
   registerFieldAngle();
   registerFieldColourSlider();
   registerFieldDropdown();
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
-const path = require('path');
+const path = require("path");
 
 // Base config that applies to either development or production mode.
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.ts",
   output: {
-    library: 'ScratchBlocks',
-    libraryTarget: 'commonjs2',
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    library: "ScratchBlocks",
+    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   // Enable webpack-dev-server to get hot refresh of the app.
   devServer: {
-    static: './build',
+    static: "./build",
   },
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
+  if (argv.mode === "development") {
     // Set the output path to the `build` directory
     // so we don't clobber production builds.
-    config.output.path = path.resolve(__dirname, 'build');
+    config.output.path = path.resolve(__dirname, "build");
 
     // Generate source maps for our code for easier debugging.
     // Not suitable for production builds. If you want source maps in
     // production, choose a different one from https://webpack.js.org/configuration/devtool
-    config.devtool = 'eval-cheap-module-source-map';
+    config.devtool = "eval-cheap-module-source-map";
 
     // Include the source maps for Blockly for easier debugging Blockly code.
     config.module.rules.push({
       test: /(blockly\/.*\.js)$/,
-      use: [require.resolve('source-map-loader')],
-      enforce: 'pre',
+      use: [require.resolve("source-map-loader")],
+      enforce: "pre",
     });
 
     // Ignore spurious warnings from source-map-loader
